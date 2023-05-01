3d8323205addf0ef68d2bcbf4540340ccbc1a041
Jongok Yoon <yoonjongok1209@gmail.com>
2023-05-01 15:51:41 +0200

    base box components done

---
+++ src/components/container.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import { cn } from '../lib/utils';
+import FlexBoxColumn from './ui/flexbox-column';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: LayoutProps) {
+  return (
+    <FlexBoxColumn className={cn('w-full  h-full min-h-screen dark:bg-black-900')}>
+      {children}
+    </FlexBoxColumn>
+  );
+}
+
---
+++ src/components/ui/flexbox-column.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { type VariantProps, cva } from 'class-variance-authority';
+import React, { ReactNode } from 'react';
+
+const flexBoxColumn = cva(['flex ', 'flex-col'], {
+  variants: {
+    intent: {},
+    modifier: {},
+    fullWidth: {
+      true: ['w-full'],
+    },
+    defaultVariants: {},
+  },
+});
+
+export interface FlexBoxColumnProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof flexBoxColumn> {
+  children: ReactNode;
+}
+
+export default function FlexBoxColumn({
+  className,
+  intent,
+  modifier,
+  children,
+  fullWidth,
+  ...props
+}: FlexBoxColumnProps) {
+  return (
+    <div {...props} className={flexBoxColumn({ intent, className, modifier, fullWidth })}>
+      {children}
+    </div>
+  );
+}
+
